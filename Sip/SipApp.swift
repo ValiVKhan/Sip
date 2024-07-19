686357537b3a6a12d05b000937667311fc1dea84
ValiVKhan <ValiVajih@gmail.com>
2024-07-19 14:22:54 -0400

    Initial Commit

---
+++ Sip/SipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SipApp.swift
+//  Sip
+//
+//  Created by Vali Khan on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
